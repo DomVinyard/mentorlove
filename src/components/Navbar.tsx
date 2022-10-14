dce956cc6c4362b858630204769fa64b26727dda
Dilekcn <canturkdk@gmail.com>
2022-10-14 23:03:14 +0100

    Logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import { Box,Flex,Text,Image,Heading,HStack,Link, } from '@chakra-ui/react';
-import logo from '../assets/img.png'
+import logo from '../assets/logo.png'
   
   
   function Navnew() {
     return (  
         <Box bg='#202020' px='100px' color='#FFFFFF'>
           <Flex h={16} justifyContent='space-between' alignItems={'center'} px='1'>
-          <Heading display='flex'  alignItems='center'  letterSpacing='0.5px'><Text as="span" color='white'>MENTOR </Text> <Image src={logo}  h={9} px='10px'/><Text as="span" color='whiteAlpha.700' fontSize={25}> FOR SLACK</Text></Heading>
+           <Image src={logo}  h={9} px='10px'/>
 
             <HStack
               as={'nav'}
